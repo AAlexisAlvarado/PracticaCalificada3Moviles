25e1ee4004837259cdb81d24270a4b32874d324f
Linder Hassinger <linderhassinger00@gmail.com>
2018-04-19 16:48:15 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/tecsup/sugarormapp/MainActivity.java
@@ -0,0 +1,50 @@
+package pe.edu.tecsup.sugarormapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG =MainActivity.class.getSimpleName();
+
+    private static final int REGISTER_FORM_REQUEST=100;
+
+    private RecyclerView usersList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configurando el recycler view
+        usersList=(RecyclerView)findViewById(R.id.user_list);
+        usersList.setLayoutManager(new LinearLayoutManager(this));
+
+        //Enviar datos adapter or recyclerview
+        List<User> users = UserRepository.list();
+        usersList.setAdapter(new UserAdapter(users));
+    }
+
+    public void callRegisterForm(View view){
+        startActivityForResult(new Intent(this, RegisterActivity.class),REGISTER_FORM_REQUEST);
+    }
+
+    //return from RegisterActivity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //refresh data
+        UserAdapter adapter = (UserAdapter)usersList.getAdapter();
+
+        List<User> users=UserRepository.list();
+        adapter.setUsers(users);
+        adapter.notifyDataSetChanged();
+    }
+}
